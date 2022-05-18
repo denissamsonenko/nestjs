435268bd8de67f3af7ae2d6fc7d82a06bba0e073
denissamsonenko <denissamsonenko@mail.ru>
2022-05-19 00:44:42 +0300

    added model, controller, service

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,4 +1,24 @@
 import { Injectable } from '@nestjs/common';
+import { User } from './users.model';
+import { InjectModel } from '@nestjs/sequelize';
+import { CreateUserDto } from './dto/create-user.dto';
 
 @Injectable()
-export class UsersService {}
+export class UsersService {
+  constructor(@InjectModel(User) private userRepository: typeof User) {
+  }
+
+  async createUser(dto: CreateUserDto) {
+    return await this.userRepository.create(dto);
+  }
+
+  async getAllUsers() {
+    const users = await this.userRepository.findAll();
+    return users;
+  }
+
+  async getUser() {
+    const user = await this.userRepository.findOne();
+    return user;
+  }
+}
